4507fec9bf32351f8b931ba8e4f1a06ef25bfced
gbarrbrown <gbb7899@gmail.com>
2019-01-28 18:22:58 +1300

    make english&german_words seed data

---
+++ seeds/english_words.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('english_words').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('english_words').insert([
+        {id: 1, word: 'The'},
+        {id: 2, word: 'The'},
+        {id: 3, word: 'The'}
+      ]);
+    });
+};
---
+++ seeds/german_words.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('german_words').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('german_words').insert([
+        {id: 1, word: 'Die', gender: 'Feminine'},
+        {id: 2, word: 'Das', gender: 'Neuter'},
+        {id: 3, word: 'Der', gender: 'Masculine'}
+      ]);
+    });
+};
