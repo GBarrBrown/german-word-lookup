65cb85fcfa70d271958c5a0f35a7036e82443710
gbarrbrown <gbb7899@gmail.com>
2019-01-31 10:52:52 +1300

    add translations seed data

---
+++ seeds/translations.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('translations').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('translations').insert([
+        {german_word_id: 1, english_word_id: 1},
+        {german_word_id: 2, english_word_id: 1},
+        {german_word_id: 3, english_word_id: 1}
+      ]);
+    });
+};
